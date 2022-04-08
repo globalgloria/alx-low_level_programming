636ec9d1085cb639c1ffee846ed4aa3c258e0b6a
globalgloria <echendugloria@gmail.com>
2022-04-07 22:11:50 -0700

    added changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - a function that prints a string
+ *
+ * Return: always 0 Success
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
-return 0;
+	puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }
