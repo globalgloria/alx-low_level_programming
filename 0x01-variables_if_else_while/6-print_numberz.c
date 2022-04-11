a19c0c6a6b19d5b548f9567cb0aaa74654ed79f6
globalgloria <echendugloria@gmail.com>
2022-04-10 19:15:52 -0700

    print all single digits of base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: prints all single digits of base 10
+ * Return: 0
+ */
+int main(void)
+{
+	int n = '0';
+
+	while (n <= '9')
+	{
+		putchar (n);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
