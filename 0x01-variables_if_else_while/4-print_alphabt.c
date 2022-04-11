563038d99e3e758aa05e5e72673fe0918e2f58f8
globalgloria <echendugloria@gmail.com>
2022-04-10 17:58:08 -0700

    lowercase except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: print lowercase except q and e
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+		c++
+	}
+
+	putchar('\n');
+	return (0);
+
+}
+
+
