cb0c7e83be68d854b46a033615d1daa749731dd5
globalgloria <echendugloria@gmail.com>
2022-04-12 11:58:28 -0700

    alphabet in lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet_x10 - main block
+ * Description: ten times the alphabet in lowercase
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while (i < 10)
+	{
+		c = 'a';
+		while (a <= 'z')
+		{
+			_putchar(c);
+			c++
+		}
+		_putchar('\n');
+		return (0);
+	}
+
+}
+
