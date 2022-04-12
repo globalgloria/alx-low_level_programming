8326d83cb324fc6dff4ea1dc7cedd5855ec5efae
globalgloria <echendugloria@gmail.com>
2022-04-12 09:38:59 -0700

    prints putchar followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: prints_putchar followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	printf("putchar\n");
+	return (0);
+}
