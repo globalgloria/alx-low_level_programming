c934264f918c1ec236407ece9b3867d9cd2283a2
globalgloria <echendugloria@gmail.com>
2022-04-11 18:57:18 -0700

    special-digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,7 +13,7 @@ int main(void)
 	while (d < 10)
 	{
 		c = 0;
-		while (c < 100)
+		while (c < 10)
 		{
 			if (d !=c && d < c)
 			{
@@ -25,7 +25,6 @@ int main(void)
 					putchar(',');
 					putchar(' ');
 				}
-				
 			}
 
 			c++;
