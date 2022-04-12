f3c7dc627417c6b36fd261e2dbd8cd052eb4bfe6
globalgloria <echendugloria@gmail.com>
2022-04-12 10:39:33 -0700

    alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: print alphabet in lowercase followed by a new line
+ * Return: Always 0
+ */
+int main (void)
+{
+	char c = 'a';
+
+	while ( c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
+	
+
