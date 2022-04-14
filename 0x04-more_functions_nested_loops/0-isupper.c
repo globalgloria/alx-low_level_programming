03409472f3d6e4f3cd06c6d7c5207662c0b45862
globalgloria <echendugloria@gmail.com>
2022-04-14 16:18:29 -0700

    upper case letter

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,8 +2,9 @@
 /*
  * _isupper - check for upper case letter
  * Description:check for upper case letter
- * Return:0 or 1
+ * Return: 0 or 1
  */
+
 int _isupper(int c);
 {
 	return (c >= 65 && c <= 90);
