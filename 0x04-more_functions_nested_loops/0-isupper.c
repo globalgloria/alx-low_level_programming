2e4d0221a2c47a826a9d3daf4277a7a68ef8ea26
globalgloria <echendugloria@gmail.com>
2022-04-14 16:24:29 -0700

    upper case letter

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,12 @@
 #include "main.h"
-/*
+
+/**
  * _isupper - check for upper case letter
  * Description:check for upper case letter
  * Return: 0 or 1
  */
 
-int _isupper(int c);
+int _isupper(int c)
 {
 	return (c >= 65 && c <= 90);
 }
