0a53133bb0ca19b8a6ba3c0c0889f85a4bc20c1b
globalgloria <echendugloria@gmail.com>
2022-04-14 20:03:48 -0700

    Print all numbers except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,15 +5,14 @@
  */
 void print_most_numbers(void)
 {
-	int i = 0;
+	int i;
 
-	while (i <= 9)
+	for (i = 48; i <= 57; i++)
 	{
 		if (i != 2 && i != 4)
 		{
 			_putchar(i);
 		}
-		i++;
 	}
 	_putchar('\n');
 }
