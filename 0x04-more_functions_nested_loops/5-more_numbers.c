1d302d54d7e3b70a60e39713a682af76bd92fc5b
globalgloria <echendugloria@gmail.com>
2022-04-14 22:48:19 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,16 +9,19 @@ void more_numbers(void)
 {
 	int i = 0 , j;
 
-		while(i < 10)
+		while(i <= 10)
 		{
 			j = 0;
 			while(j <= 14)
 			{
 				_putchar(j);
 				j++;
+
 			}
+
 			_putchar('\n');
 			i++;
+
 		}
 
 }
