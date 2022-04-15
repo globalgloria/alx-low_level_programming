4c569da5ab41fea2b02420da68dc1020e62988d7
globalgloria <echendugloria@gmail.com>
2022-04-15 14:02:01 -0700

    draws a diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,17 +2,23 @@
 
 /**
  * print_diagonal - draws diagonal line on the terminal
- * @n: number of times \ to be printed
+ * @n: number of times \\ to be printed
  * Return: void
  */
 
 void print_diagonal(int n)
 {
-	int i = 0;
+	int i, j;
 
 	for (i = 0; i < n; i++)
 	{
-		_putchar('\\');
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar(92);
+		if (i < (n - 1))
+			_putchar('\n');
 	}
 	_putchar('\n');
 }
