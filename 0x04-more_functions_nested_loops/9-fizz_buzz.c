2f295c52359ed0fecb472dbdba096c1af9d89177
globalgloria <echendugloria@gmail.com>
2022-04-19 17:52:05 -0700

    prints fizz buzz for 3 and 5 respectively

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,28 +6,35 @@
  */
 int main(void)
 {
-	int n;
+	int i = 1;
 
-	n = 1;
-	printf("%d", n);
-	for (n = 2; n <= 100; n++)
+	while (i <= 100);
 	{
-		if ((n % 3 == 0) && (n % 5 == 0))
+		if (i % 3 == 0) && (i % 5 == 0)
 		{
-			printf(" FizzBuzz");
+			printf("FizzBuzz");
 		}
-		else if (n % 3 == 0)
+		else if (i % 3 == 0)
 		{
-			printf(" Fizz");
+			printf("Fizz");
 		}
-		else if (n % 5 == 0)
+		else if (i % 5 == 0)
 		{
-			printf(" Buzz");
+			printf("Buzz");
 		}
 		else
 		{
-			printf(" %d", n);
+			printf("%i", i);
 		}
+
+		if (i != 100)
+		{
+			putchar(' ');
+		}
+
+		i++;
 	}
-	printf("\n");
+	putchar('\n');
 	return (0);
+
+}
