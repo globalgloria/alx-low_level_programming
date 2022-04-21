60bde3a0d517d8007a39cee032df28da7f5ab0b6
globalgloria <echendugloria@gmail.com>
2022-04-20 20:37:41 -0700

    reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,7 +6,7 @@
 void rev_string(char *s)
 {
 	char tmp;
-	int i, len, len1;
+	len, len1;
 
 	len = 0;
 	len1 = 0;
@@ -14,7 +14,7 @@ void rev_string(char *s)
 	{
 		len++;
 	}
-	len1 = len - 1:
+	len1 = len - 1;
 	for (i = 0; i < len / 2; i++)
 	{
 		tmp = s[i];
