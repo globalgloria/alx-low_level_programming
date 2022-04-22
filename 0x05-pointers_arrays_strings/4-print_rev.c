7b2bf793126a6abce8b9a2490e86b1c4efa1a27b
globalgloria <echendugloria@gmail.com>
2022-04-21 20:45:42 -0700

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,18 +3,22 @@
 /**
  * print_rev - prints a string, in reverse, followed by a new line
  * @s: pointer to the string to print
- * Return: void
  */
 
 void print_rev(char *s)
 {
-	int i = 0;
-	while (s[i])
-		i++;
+	int i, j, len;
 
-	while (i--)
+	i = 0;
+
+	while (s[i] != '\0')
 	{
-		putchar(s[i]);
+		i++;
 	}
-	putchar('\n');
+	len = i;
+	for (j = len - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
 }
