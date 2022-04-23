91baea8d393ce05e2850975a62d81853f5d3f839
globalgloria <echendugloria@gmail.com>
2022-04-23 14:47:15 -0700

    concatenates n bytes from a string

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates n bytes from a string to another
+ * @dest - destination string /  string to append
+ * @src - source string / string to add
+ * @n: number of byte of string to concatenate
+ * Return: a pointer to the resulting string dest
+ */
+
+char *strncat(char *dest, char *src, int n);
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
